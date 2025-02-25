e12a47529f840011e7f29f1affbe7b6f4dde2f0f
عبدالله الحديدي <abdallah201@protonmail.com>
2025-02-26 00:48:27 +0200

    feat: add daily scheduling for user notification command with documentation

--- routes/console.php
+++ routes/console.php
@@ -8,4 +8,15 @@ Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
 
+/**
+ * Schedules the 'app:notify-users-of-events' command to run daily at 05:00 AM.
+ * 
+ * This command is responsible for notifying users about upcoming events.
+ * It ensures timely reminders and notifications are sent to users.
+ *
+ * To verify scheduled commands, use:
+ * ```sh
+ * php artisan schedule:list
+ * ```
+ */
 Schedule::command('app:notify-users-of-events')->dailyAt('05:00');
